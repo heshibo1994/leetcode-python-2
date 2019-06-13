06fc9ffe45927453fc63a229df3d3e46811f8cdd
yif <csuheshibo@163.com>
2019-06-13 22:43:26 +0800

    2019/6/13

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
+# import re
+# a ='abc123dd45'
+# print (re.sub(r '\d+ ' ,' 100',a))
 
+
+# a={[1,2,3]:"fdsdfdsfds"}
+# print(a)
+m = [i for i in range(10) if i<5 ]
+# while True:
+#     pass
+max = 8
+print(max)
+print (r"\nwoow")
+
+
+print ('a' < 'b' )
+
+a = [1,2,3]
+b =a
+b[0] = 9
+print(a)
+
+k=9
+k+=10
+print(k)
+
+x=u=p=19
+print(x,u,p)
